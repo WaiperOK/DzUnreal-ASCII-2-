4a477b6796b46597f8f45c62852e518c9022edd7
WaiperOK <ruslan.topol2@gmail.com>
2022-07-09 14:47:20 +0300

    Добавьте файлы проекта.

---
+++ DzUnreal(ASCII-2)/DzUnreal(ASCII-2).cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+
+    std::cout << "Input a keyboard character: "; 
+
+    char n;
+    std::cin >> n;
+    std::cout << n << " has ASCII code " << static_cast<int>(n) << std::endl;
+
+    
+    std::cin >> n; 
+    std::cout << n << " has ASCII code " << static_cast<int>(n) << std::endl;
+
+    return 0;
+}
